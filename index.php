f93de3c2346552ca7e546224ef79b0946348c4b1
Elviss Kuštans <n3o59hf@gmail.com>
2016-07-01 14:19:40 +0300

    Code comments in sample

--- index.php
+++ index.php
@@ -1,34 +1,45 @@
 <?php
-
+// Autoload for MiraclClient and dependencies
 require "./vendor/autoload.php";
 
+// Read config for Miracl client
 $config = json_decode(file_get_contents("miracl.json"),true);
 
+// Initialize client
 $miracl = new MiraclClient(
     $config['client_id'],
     $config['secret'],
     $config['redirect_uri']);
 
-//Display data for tempalte
+// Model array for template
 $data = array();
 
 if (isset($_REQUEST['logout'])) {
+    // Save message in session
     flashMessage("info", "User logged out!");
     $miracl->logout();
+    // Send redirect to client page reload
     header("Location: .");
+    // End output here
     die();
 } else if (isset($_REQUEST['refresh'])) {
     $miracl->refreshUserData();
+    // Send redirect to client page reload
     header("Location: .");
+    // End output here
     die();
 } else {
+    // Reload page if authorization happened just now
     if ($miracl->validateAuthorization()) {
-        //Redirect if authorization happened just now
+        // Save message in session
         flashMessage("success", "Successfully logged in!");
+        // Send redirect to client page reload
         header("Location: .");
+        // End output here
         die();
     }
 
+    // Populate model for template
     if ($miracl->isLoggedIn()) {
         $data['isAuthorized'] = true;
         $data['email'] = $miracl->getEmail();
@@ -38,15 +49,21 @@ if (isset($_REQUEST['logout'])) {
     }
 }
 
-//Show user messages saved in session
+// Add messages to model and clear messages from session
 if (isset($_SESSION["messages"])) {
     $data["messages"] = $_SESSION["messages"];
     $_SESSION["messages"] = array();
 }
 
+// Render template from model and output that to client
 echo renderTemplate("main", $data);
 
-//save message to session for showing it later
+/**
+ * Save message to session for showing it later
+ *
+ * @param $category string Category of message. Default Bootstrap categories are: success, info, warning, danger
+ * @param $message string Message text
+ */
 function flashMessage($category, $message)
 {
     $_SESSION["messages"][] = array(
@@ -55,9 +72,15 @@ function flashMessage($category, $message)
     );
 }
 
+/**
+ * Render template and return it as string.
+ * 
+ * @param $name string File name for template
+ * @param $vars array Associative array with name-value pairs for template
+ * @return string Rendered template
+ */
 function renderTemplate($name, $vars)
 {
-
     ob_start();
     extract($vars);
     include "templates/$name.php";
